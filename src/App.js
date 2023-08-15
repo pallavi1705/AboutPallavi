e43c043ebd1545c94cd0e7535293143008618269
Pallavi Nagendra <pallavibharadwaj17@gmail.com>
2023-08-16 00:12:32 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-
 import AboutMe from "./components/aboutMe";
 import Home from "./components/home";
 import Header from "./components/header";
-import { Routes, Route, Link } from "react-router-dom";
 import Skills from "./components/skills";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-       <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<AboutMe />}></Route>
-          <Route path="/skills" element={<Skills />}></Route>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<AboutMe />}></Route>
+        <Route path="/skills" element={<Skills />}></Route>
       </Routes>
     </div>
   );
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,37 +1,31 @@
-import profileImage from "../images/profileImage.jpg";
 import introductionImg from "../images/backgroundImage.jpg";
 
 function Home() {
   return (
     <>
-    
-
-
-    <div className="card">
-      <div className="card bg-white text-dark">
-        <img
-          className="card-img"
-          src={introductionImg}
-          alt="Introduction Img"
-        />
-        <div className="card-img-overlay">
-          <h5
-            className="card-title font-italic text-lg-center"
-            style={{ color: "#800080" }}
-          >
-            Hi there, I'm Pallavi!!
-          </h5>
-          <p className="card-text text-lg-right font-weight-light">
-            I am a daughter, a sister and a passionate Engineer who loves to
-            translate ideas into clean and creative websites for a great user
-            experience
-          </p>
+      <div className="card">
+        <div className="card bg-white text-dark">
+          <img
+            className="card-img"
+            src={introductionImg}
+            alt="Introduction Img"
+          />
+          <div className="card-img-overlay">
+            <h5
+              className="card-title font-italic text-lg-center"
+              style={{ color: "#800080" }}
+            >
+              Hi there, I'm Pallavi!!
+            </h5>
+            <p className="card-text text-lg-right font-weight-light">
+              I am a daughter, a sister and a passionate Engineer who loves to
+              translate ideas into clean and creative websites for a great user
+              experience
+            </p>
+          </div>
         </div>
       </div>
-      
-    </div>
     </>
-    
   );
 }
 
