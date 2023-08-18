4e08b7fed34e5bfce71785f9d8c9d6a6d74d3333
Pallavi Nagendra <pallavibharadwaj17@gmail.com>
2023-08-18 13:00:09 +0530

    Final Contact Updated

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -4,14 +4,6 @@ function AboutMe() {
   return (
     <>
       <div className="row g-0 m-2">
-        {/* <div className="col-md-2">
-          <img
-            src={profileImage}
-            alt="profileImg"
-            style={{ borderRadius: "50%" }}
-            className="img-thumbnail"
-          />
-        </div> */}
         <div className="col-md-10">
           <div className="card-body">
             <h5 className="card-title font-italic" style={{ color: "#800080" }}>
@@ -52,10 +44,10 @@ function AboutMe() {
       <div className="container">
         <div
           className="card p-2 m-2"
-          style={{ width: "500px", height: "50px" }}
+          style={{ width: "500px", height: "60px" }}
         >
           <div className="row">
-            <div className="col-sm-3">
+            <div className="col-sm-2">
               <img
                 className="card-img"
                 src="https://tse3.mm.bing.net/th?id=OIP.7y88DqLREW8ApbxWuMBmzgHaEo&pid=Api&P=0&h=180"
@@ -63,7 +55,7 @@ function AboutMe() {
                 style={{ width: "60px", height: "35px" }}
               />
             </div>
-            <div className="col-sm-9">
+            <div className="col-sm-10">
               <div className="card-body-right">
                 <a
                   className="card-title font-italic"
@@ -76,16 +68,19 @@ function AboutMe() {
               </div>
             </div>
           </div>
+
+
+          
         </div>
       </div>
 
       <div className="container">
         <div
           className="card p-2 m-2"
-          style={{ width: "500px", height: "50px" }}
+          style={{ width: "500px", height: "60px" }}
         >
           <div className="row">
-            <div className="col-sm-3">
+            <div className="col-sm-2">
               <img
                 className="card-img"
                 src="https://tse2.mm.bing.net/th?id=OIP._HIgvWNkUXHN01X7N42A_wHaHa&pid=Api&P=0&h=180"
@@ -93,7 +88,7 @@ function AboutMe() {
                 style={{ width: "25px", height: "25px" }}
               />
             </div>
-            <div className="col-sm-9">
+            <div className="col-sm-10">
               <div className="card-body-right">
                 <a
                   className="card-title font-italic"
@@ -112,10 +107,10 @@ function AboutMe() {
       <div className="container">
         <div
           className="card p-2 m-2"
-          style={{ width: "500px", height: "50px" }}
+          style={{ width: "500px", height: "60px" }}
         >
           <div className="row">
-            <div className="col-sm-3">
+            <div className="col-sm-2">
               <img
                 className="card-img"
                 src="https://tse3.mm.bing.net/th?id=OIP.wEDp5KI2DzQ7uLI8NMt48AHaHf&pid=Api&P=0&h=180"
@@ -123,7 +118,7 @@ function AboutMe() {
                 style={{ width: "25px", height: "25px" }}
               />
             </div>
-            <div className="col-sm-9">
+            <div className="col-sm-10">
               <div className="card-body-right">
                 <p className="card-title font-italic text-primary">
                   +91-9611318393
